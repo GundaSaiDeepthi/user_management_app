66418e15289ac27beeb2292d97188adb86b04a8b
GundaSaiDeepthi <23eg105r52@anurag.edu.in>
2026-05-14 11:01:44 +0530

    Updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer
+      style={{
+        backgroundColor: '#222',
+        color: '#fff',
+        textAlign: 'center',
+        padding: '15px',
+        marginTop: '20px'
+      }}
+    >
+      <p>© 2026 User Management App. All Rights Reserved.</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div
+      style={{
+        textAlign: 'center',
+        padding: '50px',
+        minHeight: '100vh'
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '48px',
+          color: '#155724'
+        }}
+      >
+        Welcome to User Management App
+      </h1>
+
+      <p
+        style={{
+          fontSize: '24px',
+          color: '#2e7d32',
+          marginTop: '20px'
+        }}
+      >
+        Add users and See users list.
+      </p>
+
+     
+    </div>
+  );
 }
 
-export default Home
+export default Home;
